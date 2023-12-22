43a41b514e2527da51fddb9907a3d3349001dd10
ömer utku demir <120313399+omerutkudemir@users.noreply.github.com>
2023-12-22 22:49:09 +0300

    Add files via upload

---
+++ hospital_oop/src/main/java/com/cbu/hospital_oop/model/Yonetim.java
@@ -0,0 +1,38 @@
+package com.cbu.hospital_oop.model;
+
+public class Yonetim extends User{
+
+    public void giris_yap()
+    {
+
+    }
+    public void doktorlari_goruntule()
+    {
+
+    }
+
+    public void calisma_izni_karar()
+    {
+
+    }
+
+    public void hastalari_goruntule()
+    {
+
+    }
+    public void hangi_doktor_hangi_hastalara_bakmis()
+    {
+
+    }
+
+    public void hangi_hastaya_hangi_tedavi_uygulanmis()
+    {
+
+    }
+
+    public void cikis_yap()
+    {
+
+    }
+
+}
