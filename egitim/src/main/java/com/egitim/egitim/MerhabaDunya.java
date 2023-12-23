38c6893175ee0b9e738979afad6149dfdb438521
Omer Utku Demir <utkudemir60@gmail.com>
2023-12-23 21:02:34 +0300

    message

--- egitim/src/main/java/com/egitim/egitim/MerhabaDunya.java
+++ egitim/src/main/java/com/egitim/egitim/MerhabaDunya.java
@@ -1,2 +1,18 @@
-package com.egitim.egitim;public class MerhabaDunya {
+package com.egitim.egitim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MerhabaDunya extends SpringBootServletInitializer {
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(MerhabaDunya.class);
+        }
+
+        public static void main(String[] args) throws Exception {
+            SpringApplication.run(MerhabaDunya.class, args);
+        }
 }
